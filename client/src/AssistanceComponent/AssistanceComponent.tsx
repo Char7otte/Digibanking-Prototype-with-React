6494cc51093ea2fcbf90298a8b67a418cd77b20a
shar <faethychan@gmail.com>
2025-11-13 17:41:45 +0800

    Add assistance modal

--- client/src/AssistanceComponent/AssistanceComponent.tsx
+++ client/src/AssistanceComponent/AssistanceComponent.tsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+import ReactModal from "react-modal";
 import styles from "./AssistanceComponent.module.css";
 
 function AssistanceComponent() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
     return (
         <div className={styles.assistanceButtonContainer}>
-            <button>Assistance</button>
+            <button onClick={openModal}>Assistance♿</button>
+            <ReactModal isOpen={isOpen} onRequestClose={closeModal}>
+                <label htmlFor="">
+                    Distance between buttons: <strong>{elementDistance}px</strong>
+                </label>{" "}
+                <br />
+                <input type="range" className={styles.rangeInput} min="16" max="48" onChange={handleDistanceChange} value={elementDistance} />
+                <hr />
+            </ReactModal>
         </div>
     );
 }
