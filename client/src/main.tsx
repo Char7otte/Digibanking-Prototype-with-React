1ff5ae7d78f403c46598a9b5e8ec619c2373c94d
shar <faethychan@gmail.com>
2025-11-13 18:09:57 +0800

    Add routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import Dashboard from "./Dashboard/Dashboard.tsx";
+import NotFound from "./NotFound/NotFound.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    { path: "/", element: <App /> },
+    { path: "/dashboard", element: <Dashboard /> },
+    { path: "*", element: <NotFound /> },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
