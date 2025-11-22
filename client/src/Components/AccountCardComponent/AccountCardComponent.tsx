ddfa870371bdcc0309e5316992598897863c940c
shar <faethychan@gmail.com>
2025-11-23 02:31:13 +0800

    Save account number in article to pass along

--- client/src/Components/AccountCardComponent/AccountCardComponent.tsx
+++ client/src/Components/AccountCardComponent/AccountCardComponent.tsx
@@ -23,7 +23,7 @@ function AccountCardComponent({ accountData: { type, currency, number, money, is
     }
 
     return (
-        <article className={`${styles.mainContainer} spacing-md accountCard`}>
+        <article className={`${styles.mainContainer} spacing-md accountCard`} data-account-number={number}>
             <div className="d-flex justify-content-between">
                 <div>
                     <h3>{type}</h3>
