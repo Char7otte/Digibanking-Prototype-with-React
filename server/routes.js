701ec58f3f4b29d7372c74b62a67bd1de9260091
shar <faethychan@gmail.com>
2026-01-16 03:04:48 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,14 @@ const dashboardController = require("./api-mvc/controllers/dashboardController")
 const profileController = require("./api-mvc/controllers/profileController");
 const transferController = require("./api-mvc/controllers/transferController");
 
+router.get("/", (req, res) => {
+  res.send("Hello from Vercel");
+});
+
+router.get("/api/data", (req, res) => {
+  res.json({ message: "API is working" });
+});
+
 // Login action
 router.post("/login", loginController.login);
 
@@ -18,13 +26,13 @@ router.post("/profile/update", profileController.updateProfile);
 
 // Transfer page
 router.get("/transfer", (req, res) => {
-    if (!req.session.user) return res.redirect("/login");
+  if (!req.session.user) return res.redirect("/login");
 
-    res.render("transfer", {
-        error: null,
-        success: null,
-        user: req.session.user,
-    });
+  res.render("transfer", {
+    error: null,
+    success: null,
+    user: req.session.user,
+  });
 });
 
 // Transfer action
@@ -32,9 +40,9 @@ router.post("/transfer", transferController.transfer);
 
 // Logout
 router.get("/logout", (req, res) => {
-    req.session.destroy(() => {
-        res.redirect("/");
-    });
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
