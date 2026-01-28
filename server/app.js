af92039dec185ae0ba100eb66db0023a1b1f7530
shar <faethychan@gmail.com>
2026-01-28 14:46:29 +0800

    Hide CORS origin

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(
   }),
 );
 app.use(morgan("dev"));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // STATIC FILES
 app.use(express.static(path.join(__dirname, "public")));
