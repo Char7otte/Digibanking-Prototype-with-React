3e7a956a2c692b8e519d0632ecf3a8a251d0908f
shar <faethychan@gmail.com>
2026-01-28 15:06:05 +0800

    Update API BASE URL to use vite prefix

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -20,7 +20,8 @@ function LoginComponent() {
       password: formData.get("password"),
     };
 
-    const API_BASE_URL = import.meta.env.API_URL || "http://localhost:8080";
+    const API_BASE_URL =
+      import.meta.env.VITE_API_URL || "http://localhost:8080";
 
     try {
       setIsLoading(true);
